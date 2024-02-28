43f6859cb56dfee770c96d149b3384bb73f21f35
Micke Nordin <kano@sunet.se>
2024-02-28 11:26:44 +0100

    site_selector: Add redirect capabilities
    This patch makes it possible to configure a redirect_url, that will be called if no access rule matches.
    The redirect will also get a payload that looks like this:
    
    {
        "user_id": internal_data.subject_id,
        "displayname": (internal_data.attribute.get("displayname") or ["Unknown"])[0],
        "timestamp": internal_data.auth_info.timestamp,
        "issuer": internal_data.auth_info.issuer,
        "service": service_id,
        "session_id": context.state.session_id
    }
    
    If the redirect_url is not configured, the old behaviour with an exception is kept.

--- src/swamid_plugins/site_selector/__init__.py
+++ src/swamid_plugins/site_selector/__init__.py
@@ -1,7 +1,9 @@
 from re import match as match_regex
+from urllib.parse import urlencode
 
 from satosa.exception import SATOSAError
 from satosa.micro_services.base import ResponseMicroService
+from satosa.response import Redirect
 
 
 class Error(SATOSAError):
@@ -9,21 +11,19 @@ class Error(SATOSAError):
 
 
 class SiteSelector(ResponseMicroService):
+
     def __init__(self, config, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # TODO: validate configuration format
         self.access_rules = config.get("access_rules", {})
-        self.access_rules_default = (
-            self.access_rules.pop("", None)
-            or self.access_rules.pop("default", None)
-        )
+        self.redirect_url = config.get("redirect_url", None)
+        self.access_rules_default = (self.access_rules.pop("", None)
+                                     or self.access_rules.pop("default", None))
 
     def process(self, context, internal_data):
         service_id = internal_data.requester
-        access_rules_for_service = (
-            self.access_rules.get(service_id)
-            or self.access_rules_default
-        )
+        access_rules_for_service = (self.access_rules.get(service_id)
+                                    or self.access_rules_default)
 
         # evaluate each rule set for this particular service
         for rule in access_rules_for_service:
@@ -32,21 +32,29 @@ class SiteSelector(ResponseMicroService):
             if result:
                 return super().process(context, internal_data)
 
-        # no rule was successful; abort processing the response any further
-        error_context = {
-            "message": "Failed to satisfy any rule for accessing the service.",
+        # no rule was successful; either redirect or abort processing the response any further
+        if not self.redirect_url:
+            error_context = {
+                "message":
+                "Failed to satisfy any rule for accessing the service.",
+                "service": service_id,
+                "rules": access_rules_for_service,
+            }
+            raise Error(error_context)
+        query_string = urlencode({
+            "user_id": internal_data.subject_id,
+            "displayname": (internal_data.attribute.get("displayname") or ["Unknown"])[0],
+            "timestamp": internal_data.auth_info.timestamp,
+            "issuer": internal_data.auth_info.issuer,
             "service": service_id,
-            "rules": access_rules_for_service,
-        }
-        raise Error(error_context)
+            "session_id": context.state.session_id
+        })
+        return Redirect(self.redirect_url + f'?{query_string}')
 
 
 def evaluate_rule(rule, attributes):
     regex = rule["match"]
     attr = rule["attribute"]
     attr_values = attributes.get(attr, [])
-    satisfied = any(
-        match_regex(regex, v)
-        for v in attr_values
-    )
+    satisfied = any(match_regex(regex, v) for v in attr_values)
     return satisfied
